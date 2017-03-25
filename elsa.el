3375ba2512bd4f5f56a6f246fe697c65d8050782
Matus Goljer <matus.goljer@gmail.com>
2017-03-25 14:00:13 +0100

    Refactor how defuns store the arguments.

--- elsa.el
+++ elsa.el
@@ -47,14 +47,17 @@
 
 (defclass elsa-defun nil
   ((name :initarg :name)
-   (return-type :initarg :return-type)
-   (arg-types :initarg :arg-types)))
+   (args :initarg :args)
+   (return-type :initarg :return-type)))
+
+(defclass elsa-expression nil
+  ((type :initarg :type)))
 
 (defmethod elsa-state-add-defvar ((this elsa-state) name type)
   (let ((defvars (oref this defvars)))
     (puthash name (elsa-defvar "" :name name :type type) defvars)))
 
-(defun elsa-type-definition-to-type (definition)
+(defun elsa-make-type (definition)
   "Return instance of class representing DEFINITION."
   (let* ((name (split-string (symbol-name definition) "?"))
          (class (intern (concat "elsa-type-"  (car name)))))
@@ -63,28 +66,31 @@
 (defun elsa-defun-get-return-type (declarations)
   "Get return type from DECLARATIONS."
   (-when-let (return-type (cadr (--first (eq (car it) 'elsa-return) declarations)))
-    (elsa-type-definition-to-type return-type)))
+    (elsa-make-type return-type)))
 
-(defun elsa-defun-get-arg-types (declarations)
-  "Get return type from DECLARATIONS."
-  (-when-let (arg-types (cdr (--first (eq (car it) 'elsa-args) declarations)))
-    (-map
-     (lambda (type)
-       (elsa-type-definition-to-type type))
-     arg-types)))
+(defun elsa--get-typed-args (args types)
+  "Attach types to ARGS according to TYPES."
+  (let ((re nil))
+    (-each args
+      (lambda (arg)
+        (if (memq arg '(&optional &rest &key))
+            (push (list arg) re)
+          (push (cons arg
+                      (elsa-make-type
+                       (if (consp types) (car types) 'mixed)))
+                re)
+          (!cdr types))))
+    (nreverse re)))
 
-(defmethod elsa-state-add-defun ((this elsa-state) name declarations)
+(defmethod elsa-state-add-defun ((this elsa-state) name args declarations)
   (let ((defuns (oref this defuns)))
     (puthash
      name
      (elsa-defun
       ""
-      :name name
-      :return-type (elsa-defun-get-return-type declarations)
-      :arg-types (elsa-defun-get-arg-types declarations))
-     defuns)
-    ;; (elsa-defun-get-arg-types declarations)
-    ))
+      :name name :args args
+      :return-type (elsa-defun-get-return-type declarations))
+     defuns)))
 
 (defmethod elsa-state-add-error ((this elsa-state) error)
   (oset this errors (cons error (oref this errors))))
@@ -105,48 +111,63 @@
     (oset state errors (nreverse (oref state errors)))
     state))
 
-(defun elsa-check-function (arg-types args state)
-  "Check if function accepts correct arguments.
-ARG-TYPES, ARGS, STATE."
-  (let ((defvars (oref state defvars)))
-    (-map
-     (-lambda ((type . arg))
-       (let ((other
-              (cond
-               ((and (symbolp arg)
-                     (gethash arg defvars))
-                (let ((defv (gethash arg defvars)))
-                  (oref defv type)))
-               ((stringp arg)
-                (elsa-type-string ""))
-               (t arg))))
-         (cond
-          ((not (elsa-type-child-p other))
-           (elsa-state-add-error
-            state
-            (format "Unknown type, has %S, expected %s"
-                    other (elsa-type-describe type))))
-          ((not (elsa-type-accept type other))
-           (elsa-state-add-error
-            state
-            (format "Invalid type, has %s, expected %s"
-                    (elsa-type-describe other)
-                    (elsa-type-describe type)))))))
-     (-zip arg-types args))))
+(defun elsa--get-type-of-argument (arg defvars)
+  "Get type of argument ARG in a function call form.
 
-(defun elsa-analyse-function-call (form state)
+DEFVARS contains the globally defined variables."
+  (cond
+   ((and (symbolp arg)
+         (gethash arg defvars))
+    (oref (gethash arg defvars) type))
+   ((stringp arg)
+    (elsa-make-type 'string))
+   (t (elsa-make-type 'mixed))))
+
+(defun elsa-check-function (state types args)
+  "Check if function accepts correct arguments.
+STATE, TYPES, ARGS"
+  (let ((defvars (oref state defvars)))
+    (-each args
+      (-lambda (arg)
+        (let ((type (cdar types))
+              (other (elsa--get-type-of-argument arg defvars)))
+          (cond
+           ((not (elsa-type-accept type other))
+            (elsa-state-add-error
+             state
+             (format "Invalid type, has %s, expected %s"
+                     (elsa-type-describe other)
+                     (elsa-type-describe type))))))
+        ;; TODO: handle optional and keyword args
+        (!cdr types)))))
+
+(defun elsa-analyse-function-call (state form)
   "Analyse function call FORM in STATE."
   (let ((defuns (oref state defuns)))
     (-when-let (def (gethash (car form) defuns))
-      (elsa-check-function (oref def arg-types) (cdr form) state))))
+      (elsa-check-function state (oref def args) (cdr form)))))
+
+(defun elsa-analyse-defun (state name args body &optional declarations)
+  "Add function to STATE under NAME.
+
+ARGS is the arglist which might be enriched with types provided
+in DECLARATIONS (or mixed type by default).
+
+BODY is the body of the function which is further analysed."
+  (elsa-state-add-defun
+   state
+   name
+   (elsa--get-typed-args
+    args (cdr (--first (eq (car it) 'elsa-args) declarations)))
+   declarations))
 
 (defun elsa-analyse-form (form state &optional type)
   "Analyse FORM in STATE.
 
 If TYPE is non-nil, force this type on FORM."
   (pcase form
-    (`(elsa-type ,type ,form)
-     (elsa-analyse-form form state (elsa-type-definition-to-type type)))
+    (`(elsa-cast ,type ,form)
+     (elsa-analyse-form form state (elsa-make-type type)))
     ;; TODO: handle type
     (`(defvar ,name . ,_)
      (elsa-state-add-defvar state name (or type (elsa-type-mixed ""))))
@@ -157,10 +178,14 @@ If TYPE is non-nil, force this type on FORM."
     (`(defcustom ,name ,_ . ,_)
      (elsa-state-add-defvar state name (or type (elsa-type-mixed ""))))
     ;; TODO: add type inference to get return type from exit forms
-    (`(defun ,name ,_ ,(pred stringp) (declare . ,declarations) . ,_)
-     (elsa-state-add-defun state name declarations))
+    (`(defun ,name ,args ,(pred stringp) (declare . ,declarations) . ,body)
+     (elsa-analyse-defun state name args body declarations))
+    (`(defun ,name ,args ,(pred stringp) . ,body)
+     (elsa-analyse-defun state name args body))
+    (`(defun ,name ,args . ,body)
+     (elsa-analyse-defun state name args body))
     (`(,function-name . ,rest)
-     (elsa-analyse-function-call (cons function-name rest) state))))
+     (elsa-analyse-function-call state (cons function-name rest)))))
 
 (provide 'elsa)
 ;;; elsa.el ends here
