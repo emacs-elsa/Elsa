5e900d5a48c8d3133ce108cdaeed7e8d5a448bbe
Matus Goljer <matus.goljer@gmail.com>
2018-08-05 15:12:57 +0200

    Add documentation to elsa-check

--- elsa-check.el
+++ elsa-check.el
@@ -5,10 +5,22 @@
     (unless (--any? (eq (oref it name) name) elsa-checks)
       (push check elsa-checks))))
 
-(defclass elsa-check () () :abstract t)
+(defclass elsa-check () () :abstract t
+  :documentation
+  "A check is essentially a callback run at every node of the AST.
 
-(cl-defgeneric elsa-check-should-run ((this elsa-check) form scope) nil)
+A check has to implement two methods:
 
-(cl-defgeneric elsa-check-check ((this elsa-check) form scope) nil)
+- `elsa-check-should-run' to decide if the check should process
+  the current node.
+- `elsa-check-check' which performs the actual computation.")
+
+(cl-defmethod elsa-check-should-run ((this elsa-check) form scope)
+  "Decide if the check should run on FORM."
+  nil)
+
+(cl-defmethod elsa-check-check ((this elsa-check) form scope)
+  "Run THIS check on FORM in SCOPE."
+  nil)
 
 (provide 'elsa-check)
