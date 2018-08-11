84c0d1223731976ff2573c8ffba54fb928ad6603
Matus Goljer <matus.goljer@gmail.com>
2018-08-11 15:12:18 +0200

    Initialize test buffers in emacs-lisp-mode

--- tests/elsa-test-helpers.el
+++ tests/elsa-test-helpers.el
@@ -1,6 +1,7 @@
 (defmacro elsa-test-with-buffer (initial &rest body)
   (declare (indent 1))
   `(with-temp-buffer
+     (emacs-lisp-mode)
      (insert ,initial)
      (goto-char (point-min))
      (when (search-forward "|" nil t)
