a55deb54a7e2f0feaf9a741da15a7585d039791f
Matus Goljer <matus.goljer@gmail.com>
2018-08-16 16:16:17 +0200

    Make it possible to pull state out of `elsa-test-with-analysed-form` macro

--- tests/elsa-test-helpers.el
+++ tests/elsa-test-helpers.el
@@ -16,10 +16,13 @@
 
 (defmacro elsa-test-with-analysed-form (initial form-var &rest body)
   (declare (indent 2))
-  `(elsa-test-with-buffer ,initial
-     (let ((,form-var (elsa-read-form))
-           (state (elsa-state)))
-       (elsa--analyse-form ,form-var (oref state scope) state)
-       ,@body)))
+  (let ((state (if (eq (car body) :state-var)
+                   (cadr body)
+                 (make-symbol "state"))))
+    `(elsa-test-with-buffer ,initial
+       (let ((,form-var (elsa-read-form))
+             (,state (elsa-state)))
+         (elsa--analyse-form ,form-var (oref ,state scope) ,state)
+         ,@body))))
 
 (provide 'elsa-test-helpers)
