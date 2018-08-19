ca09dc6068ff759e815c36d015b61beccdc77e8f
Matus Goljer <matus.goljer@gmail.com>
2018-08-19 16:18:33 +0200

    Add tests for narrowing variables in if

--- tests/test-analyser.el
+++ tests/test-analyser.el
@@ -18,4 +18,20 @@
 
     (it "should keep the spec as provided otherwise"
       (elsa-test-with-analysed-form "|(fun a b c d)" form
-        (expect (elsa--analyse-normalize-spec (list nil t nil t) form) :to-equal (list nil t nil t))))))
+        (expect (elsa--analyse-normalize-spec (list nil t nil t) form) :to-equal (list nil t nil t)))))
+
+
+  (describe "Narrowing types in if"
+
+
+    (it "should narrow a variable to its type in true body"
+      (elsa-test-with-analysed-form "|(defun fn (x) (if x x))" form
+        (let ((second-cond (elsa-nth 2 (elsa-nth 3 form))))
+          (expect (oref second-cond type) :to-be-type-equivalent
+                  (elsa-type-diff (elsa-type-mixed) (elsa-type-nil))))))
+
+    (it "should narrow a variable to nil in else body"
+      (elsa-test-with-analysed-form "|(defun fn (x) (if x x x))" form
+        (let ((second-cond (elsa-nth 3 (elsa-nth 3 form))))
+          (expect (oref second-cond type) :to-be-type-equivalent
+                  (elsa-type-nil)))))))
