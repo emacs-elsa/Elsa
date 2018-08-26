0d58601f06ccea9d7ec0928112d43061fccd320d
Matus Goljer <matus.goljer@gmail.com>
2018-08-26 18:29:30 +0200

    Add test file for variables

---
+++ tests/test-variable.el
@@ -0,0 +1,25 @@
+;; -*- lexical-binding: t -*-
+
+(require 'elsa-undercover)
+(require 'elsa-types)
+
+(describe "Elsa Variables"
+
+  (describe "elsa-variables-group-and-sum"
+
+    (it "should group variables by name and sum the types"
+      (let* ((a1 (elsa-variable :name 'a :type (elsa-type-int)))
+             (a2 (elsa-variable :name 'a :type (elsa-type-string)))
+             (b1 (elsa-variable :name 'b :type (elsa-type-float)))
+             (b2 (elsa-variable :name 'b :type (elsa-type-bool)))
+             (grouped (elsa-variables-group-and-sum (list a1 a2 b1 b2))))
+        (expect (length grouped) :to-be 2)
+        (expect (oref (car grouped) name) :to-be 'a)
+        (expect (elsa-type-equivalent-p
+                 (oref (car grouped) type)
+                 (elsa-make-type String | Int))
+                :to-be-truthy)
+        (expect (elsa-type-equivalent-p
+                 (oref (cadr grouped) type)
+                 (elsa-make-type Float | Bool))
+                :to-be-truthy)))))
