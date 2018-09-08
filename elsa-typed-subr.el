de644210d3d21f7810c244db5b39882571582831
Matus Goljer <matus.goljer@gmail.com>
2018-09-09 00:46:17 +0200

    Add type annotation for last

--- elsa-typed-subr.el
+++ elsa-typed-subr.el
@@ -2,5 +2,6 @@
 (require 'elsa-type-helpers)
 
 (put 'match-string 'elsa-type (elsa-make-type Int -> String? -> String?))
+(put 'last 'elsa-type (elsa-make-type [Mixed] -> Int? -> Cons))
 
 (provide 'elsa-typed-subr)
