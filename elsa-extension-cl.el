aabf02f52a2ac4aaceb1d2aa0f3c1bdb17734f6f
Matus Goljer <matus.goljer@gmail.com>
2022-02-07 22:12:04 +0200

    fix(cl): add support for qualifiers on cl-defmethod

--- elsa-extension-cl.el
+++ elsa-extension-cl.el
@@ -2,15 +2,20 @@
 (require 'elsa-typed-cl)
 
 (defun elsa--analyse:cl-defmethod (form scope state)
+  "cl-defmethod is like defun, but there can be extra
+\"qualifiers\" after the method name.  These are a keywords or
+keyword-sexp pairs."
   (let* ((sequence (oref form sequence))
          (name (elsa-get-name (nth 1 sequence)))
+         (args-and-body (-drop-while (-not #'elsa-form-list-p)
+                                     (nthcdr 2 sequence)))
          (args (-map (lambda (arg)
                        (cond
                         ((elsa-form-list-p arg)
                          (elsa-car arg))
                         (t arg)))
-                     (oref (nth 2 sequence) sequence)))
-         (body (nthcdr 3 sequence)))
+                     (elsa-car args-and-body)))
+         (body (elsa-cdr args-and-body)))
     (elsa--analyse-defun-like-form name args body form scope state)))
 
 (defun elsa--analyse:cl-defgeneric (form scope state) nil)
