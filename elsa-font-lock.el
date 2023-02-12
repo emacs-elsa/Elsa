bb0e420d214ec8195abd76d0cab01da10b2a5bff
Matus Goljer <matus.goljer@gmail.com>
2023-02-12 16:37:11 +0100

    fix(fontlock): fix fontification of types after dehaskelization

--- elsa-font-lock.el
+++ elsa-font-lock.el
@@ -1,3 +1,21 @@
+(defun elsa--match-function-parens (limit)
+  "Highlight matching pair of parens for function args."
+  (when (re-search-forward "function.*?(" limit t)
+    (let ((op (1- (point)))
+          cl)
+      (backward-char)
+      (forward-sexp)
+      (when (< (point) limit)
+        (setq cl (1- (point)))
+        (set-match-data
+         (list (set-marker (make-marker) op)
+               (set-marker (make-marker) (1+ cl))
+               (set-marker (make-marker) op)
+               (set-marker (make-marker) (1+ op))
+               (set-marker (make-marker) cl)
+               (set-marker (make-marker) (1+ cl))))
+        t))))
+
 ;;;###autoload
 (defun elsa-setup-font-lock ()
   "Setup additional font-locking for Elsa forms."
@@ -5,35 +23,39 @@
   (font-lock-add-keywords
    nil
    `((,(rx (or
-            (and (group "(" (1+ (or (syntax word) (syntax symbol)))) (1+ space) (group "::") (1+ space))
-            (group (and "elsa-make-type" (1+ space)))))
-      (1 font-lock-doc-face t t)
+            (and "(" (group (1+ (or (syntax word) (syntax symbol)))) (1+ space) (group "::") (1+ space))
+            (and "(" (group (and "elsa-make-type" (1+ space))))))
+      ;; the function name in the defun annotation
+      (1 font-lock-function-name-face t t)
+      ;; the :: token separating the type
       (2 font-lock-variable-name-face t t)
-      (3 font-lock-keyword-face t t)
-      ("\\_<\\([A-Z]\\(?:\\sw\\|\\s_\\)*\\)\\_>"
+      ;; elsa-make-type for inline macro use
+      (3 font-lock-keyword-face nil t)
+      ;; highlight the types
+      ("\\(\\_<\\(?:\\sw\\|\\s_\\)+\\_>\\)"
        (save-excursion (up-list) (point))
        (re-search-backward (rx (or "::" "elsa-make-type")))
        (0 font-lock-type-face t))
-      ("\\_<\\([a-z]\\(?:\\sw\\|\\s_\\)*\\)\\_>"
-       (save-excursion (up-list) (point))
-       (re-search-backward (rx (or "::" "elsa-make-type")))
-       (0 nil t))
+      ;; highlight parens with paren-face if it is defined
       ("[][()]"
        (save-excursion (up-list) (1- (point)))
        (re-search-backward (rx (or "::" "elsa-make-type")))
-       (0 nil t))
-      ("[)]"
-       (if (nth 4 (syntax-ppss))
-           (progn
-             (up-list)
-             (backward-char 1)
-             (1+ (point)))
-         (1+ (point)))
+       (0 ,(if (facep 'parenthesis) ''parenthesis nil) t))
+      ;; highlight matching pairs of parens for function args
+      (elsa--match-function-parens
+       (save-excursion (up-list) (1- (point)))
        (re-search-backward (rx (or "::" "elsa-make-type")))
-       (0 font-lock-doc-face t))
-      (,(rx (or "->" "|"))
+       (1 nil t)
+       (2 nil t))
+      ;; highlight built-in constants
+      (,(rx symbol-start (or "nil" "t") symbol-end)
+       (save-excursion (up-list) (1- (point)))
+       (re-search-backward (rx (or "::" "elsa-make-type")))
+       (0 font-lock-constant-face t))
+      ;; highlight keywords and special type constructors
+      (,(rx "(" symbol-start (group (or "and" "or" "is" "readonly" "const")) symbol-end)
        (save-excursion (up-list) (point))
        nil
-       (0 font-lock-variable-name-face t))))))
+       (1 font-lock-keyword-face t))))))
 
 (provide 'elsa-font-lock)
