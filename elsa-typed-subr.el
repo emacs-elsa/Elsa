c31d7e9b8da9dd8b376ff75cdb9ee5cfad2d8490
Matus Goljer <matus.goljer@gmail.com>
2023-02-12 14:43:11 +0100

    feat(typed): add types for frame-configuration-p

--- elsa-typed-subr.el
+++ elsa-typed-subr.el
@@ -3,5 +3,6 @@
 
 (put 'match-string 'elsa-type (elsa-make-type (function (int (or string nil)) (or string nil))))
 (put 'last 'elsa-type (elsa-make-type (function ((list mixed) (or int nil)) (cons mixed mixed))))
+(put 'frame-configuration-p 'elsa-type (elsa-make-type (function (mixed) (is frame-configuration))))
 
 (provide 'elsa-typed-subr)
