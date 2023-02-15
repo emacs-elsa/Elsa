3e7ad4bbd9d50a9a08950c10c6f1c37c4a2a1123
Matus Goljer <matus.goljer@gmail.com>
2023-02-15 13:37:35 +0100

    chore(checks): remove unused method

--- elsa-check.el
+++ elsa-check.el
@@ -1,10 +1,5 @@
-;; (elsa-checks :: (list mixed))
-(defvar elsa-checks nil)
-
-(defun elsa-check-add (check)
-  (let ((name (oref check name)))
-    (unless (--any? (eq (oref it name) name) elsa-checks)
-      (push check elsa-checks))))
+(require 'dash)
+(require 'eieio)
 
 (defclass elsa-check () () :abstract t
   :documentation
@@ -15,6 +10,9 @@ A check has to implement two methods:
 - `elsa-check-should-run' to decide if the check should process
   the current node.
 - `elsa-check-check' which performs the actual computation.")
+
+;; (elsa-checks :: (list (struct elsa-check)))
+(defvar elsa-checks nil)
 
 (cl-defgeneric elsa-check-should-run ((this elsa-check) form scope state)
   "Decide if the check should run on FORM."
