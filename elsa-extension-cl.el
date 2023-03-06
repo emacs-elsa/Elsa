551cfe1b3bc0db1b853dfb10067c22368286aa24
Matus Goljer <matus.goljer@gmail.com>
2023-03-06 23:45:01 +0100

    chore(legalese): add legalese to cl extension

--- elsa-extension-cl.el
+++ elsa-extension-cl.el
@@ -1,6 +1,37 @@
+;;; elsa-extension-cl.el --- Elsa extension for common lisp -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023 Matúš Goljer
+
+;; Author: Matúš Goljer <matus.goljer@gmail.com>
+;; Maintainer: Matúš Goljer <matus.goljer@gmail.com>
+;; Version: 0.0.1
+;; Created:  6th March 2023
+;; Package-requires: ((dash "2.17.0"))
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'map)
+
+(require 'dash)
+
 (require 'elsa-analyser)
 (require 'elsa-typed-cl)
-
 (require 'elsa-extension-eieio)
 
 (defun elsa--cl-analyse-specifiers (args-raw)
@@ -177,3 +208,4 @@ keyword-sexp pairs."
            :arglist (list '&rest 'args)))))))
 
 (provide 'elsa-extension-cl)
+;;; elsa-extension-cl.el ends here
