7216f4b54e2e2de1c3aab821e02fff6a5c5cfeca
Matus Goljer <matus.goljer@gmail.com>
2023-03-12 14:45:22 +0100

    feat(core): handle errors from workers

--- elsa.el
+++ elsa.el
@@ -279,97 +279,144 @@ GLOBAL-STATE is the initial configuration."
                            (concat file " ...")))
       (bright-black "(skipped, already loaded)")))))
 
+;; This is heavily inspired by `buttercup--debugger'.
+(defun elsa--worker-debugger (&rest args)
+  "Retrieve the backtrace from the point of error."
+  (setq num-nonmacro-input-events (1+ num-nonmacro-input-events))
+  (throw 'elsa-debugger-continue
+         (list
+          'failed
+          (let ((frames nil)
+                (i 0)
+                (in-program-stack nil))
+            (dolist (frame (backtrace-frames))
+              (when in-program-stack
+                (push frame frames))
+              (when (eq (elt frame 1) 'elsa--worker-debugger)
+                (setq in-program-stack t)))
+            (mapconcat
+             ;; Frame is EVALD FUNC ARGS FLAGS.  Flags is
+             ;; useless so we drop it.
+             (lambda (frame)
+               (->>
+                (if (car frame)
+                    (format "  %S%s"
+                            (cadr frame)
+                            (if (nth 2 frame)
+                                (cl-prin1-to-string (nth 2 frame))
+                              "()"))
+                  (format "  (%S %s)"
+                          (cadr frame)
+                          (mapconcat
+                           (lambda (x) (format "%S" x))
+                           (nth 2 frame)
+                           " ")))
+                (replace-regexp-in-string "\n" "\\\\n")
+                (replace-regexp-in-string "%" "%%")))
+             (nreverse frames)
+             "\n")))))
+
 (defun elsa--worker-function-factory (worker-id project-directory)
   "Return function running in the Elsa analysis worker."
   (let ((load--path load-path))
     (lambda ()
-      (condition-case err
-          (progn
-            (setq load-path load--path)
-            (setq elsa-is-language-server nil)
-            (setq ansi-inhibit-ansi t)
-            (require 'elsa)
-            (require 'async)
-            (require 'lgr)
-            (-> (lgr-get-logger "elsa")
-                (lgr-reset-appenders)
-                (lgr-add-appender
-                 (-> (elsa-worker-appender)
-                     (lgr-set-layout (elsa-plain-layout))))
-                (lgr-set-threshold lgr-level-info))
-            (let ((msg nil)
-                  (lgr (lgr-get-logger "elsa.analyse.worker")))
-              (setq elsa-global-state (elsa-global-state))
-              (oset elsa-global-state project-directory project-directory)
-              (oset elsa-global-state number-of-files 1)
-              (oset elsa-global-state processed-file-index 1)
-              (catch 'done
-                (while t
-                  (setq msg (async-receive))
-                  (let ((op (plist-get msg :op)))
-                    (cond
-                     ((equal op "analyze")
-                      (let* ((dep (plist-get msg :dep))
-                             (library (plist-get msg :file))
-                             (current-time (current-time))
-                             (elsa-cache-file (elsa--get-cache-file-name elsa-global-state dep)))
-                        (elsa--autoload-types elsa-global-state dep)
-                        (elsa--autoload-extension elsa-global-state dep)
-                        (lgr-debug lgr "Starting analysis of %s" dep)
-                        (let ((state (elsa-process-file library elsa-global-state)))
-                          ;; `subr' has no provide for some circular
-                          ;; dependency "bootstrap" issues.  We add it here
-                          ;; artificially.
-                          (lgr-debug lgr "Analysis of %s done" dep)
-                          (when (equal dep "subr")
-                            (oset state provide (list 'subr)))
-                          (elsa-save-cache state elsa-global-state)
-                          (lgr-debug lgr "Finished saving cache for %s" dep))
-                        (async-send :ack "ok" :op op
-                                    :dep dep
-                                    :file library
-                                    :duration (float-time
-                                               (time-subtract
-                                                (current-time) current-time)))))
-                     ((equal op "load-from-cache")
-                      (let ((files (plist-get msg :files)))
-                        (dolist (file files) (load (f-no-ext file) t t))
-                        (async-send :ack "ok" :op op)))
-                     ((equal op "quit")
-                      (throw 'done t))))))))
-        (error (async-send :ack "error" :error err :trace (progn (require 'backtrace) (backtrace-to-string))))))))
+      (setq load-path load--path)
+      (require 'elsa)
+      (-> (lgr-get-logger "elsa")
+          (lgr-reset-appenders)
+          (lgr-add-appender
+           (-> (elsa-worker-appender)
+               (lgr-set-layout (lgr-layout-format :format "[%K] %m"))))
+          (lgr-set-threshold lgr-level-warn))
+      (let ((result nil))
+        (let ((lgr (lgr-get-logger "elsa.analyse.worker"))
+              (msg nil)
+              ;; this debugger stuff is used to reconstruct the
+              ;; backtrace to the point of error
+              (debugger #'elsa--worker-debugger)
+              (debug-on-error t))
+          (setq
+           result
+           (catch 'elsa-debugger-continue
+             (setq elsa-is-language-server nil)
+             (setq ansi-inhibit-ansi t)
+             (setq elsa-global-state (elsa-global-state))
+             (oset elsa-global-state project-directory project-directory)
+             (oset elsa-global-state number-of-files 1)
+             (oset elsa-global-state processed-file-index 1)
+             (catch 'elsa-worker-done
+               (while t
+                 (setq msg (async-receive))
+                 (let ((op (plist-get msg :op)))
+                   (cond
+                    ((equal op "analyze")
+                     (let* ((dep (plist-get msg :dep))
+                            (library (plist-get msg :file))
+                            (current-time (current-time))
+                            (elsa-cache-file (elsa--get-cache-file-name elsa-global-state dep)))
+                       (elsa--autoload-types elsa-global-state dep)
+                       (elsa--autoload-extension elsa-global-state dep)
+                       (lgr-debug lgr "Starting analysis of %s" dep)
+                       (let ((state (elsa-process-file library elsa-global-state)))
+                         ;; `subr' has no provide for some circular
+                         ;; dependency "bootstrap" issues.  We add it here
+                         ;; artificially.
+                         (lgr-debug lgr "Analysis of %s done" dep)
+                         (when (equal dep "subr")
+                           (oset state provide (list 'subr)))
+                         (elsa-save-cache state elsa-global-state)
+                         (lgr-debug lgr "Finished saving cache for %s" dep))
+                       (async-send :ack "ok" :op op
+                                   :dep dep
+                                   :file library
+                                   :duration (float-time
+                                              (time-subtract
+                                               (current-time) current-time)))))
+                    ((equal op "load-from-cache")
+                     (let ((files (plist-get msg :files)))
+                       (dolist (file files) (load (f-no-ext file) t t))
+                       (async-send :ack "ok" :op op)))
+                    ((equal op "quit")
+                     (lgr-warn lgr "Received signal to quit")
+                     (throw 'elsa-worker-done t)))))))))
+        (when (and (listp result)
+                   (eq (car result) 'failed))
+          (async-send :ack "error" :error (cadr result)))
+        result))))
 
 (defun elsa--parent-function-factory (worker-id workers-state max-file-name-length global-state)
   "Function handling child-to-parent messages and worker exit."
   (let ((lgr (lgr-get-logger "elsa.analyse.parent")))
     (lambda (result)
-      (if (async-message-p result)
-          (let* ((worker-state (assoc worker-id workers-state))
-                 (op (plist-get result :op))
-                 (ack (plist-get result :ack)))
-            (cond
-             ((equal ack "error")
-              (error "Error in child process %d %s" worker-id (plist-get result :error)))
-             ((equal op "echo")
-              (elsa-log
-               (with-ansi
-                (red "Worker %d said: " worker-id)
-                (yellow (plist-get result :message)))))
-             ((equal op "analyze")
-              (let* ((dep (plist-get result :dep))
-                     (file (plist-get result :file))
-                     (duration (plist-get result :duration))
-                     (elsa-cache-file (elsa--get-cache-file-name global-state dep)))
-                (elsa-log
-                 (elsa--processing-line global-state "done" file worker-id duration))
-                (load (f-no-ext elsa-cache-file) t t))
-              (cl-incf (oref global-state processed-file-index))
-              (setf (cdr worker-state) (plist-put (cdr worker-state) :ready t)))
-             ((equal op "load-from-cache")
-              (lgr-debug lgr "Worker %s loaded all cache files for this layer" worker-id)
-              (setf (cdr worker-state) (plist-put (cdr worker-state) :ready t)))))
-        (lgr-debug lgr "Async process done in worker %d, result: %s" worker-id result)
-        t))))
+      (let ((worker-state (assoc worker-id workers-state)))
+        (if (async-message-p result)
+            (let* ((op (plist-get result :op))
+                   (ack (plist-get result :ack)))
+              (cond
+               ((equal ack "error")
+                (lgr-fatal lgr (plist-get result :error))
+                (kill-emacs 1))
+               ((equal op "echo")
+                (lgr-debug lgr
+                  (with-ansi
+                   (red "Worker %d said: " worker-id)
+                   (yellow (plist-get result :message)))))
+               ((equal op "analyze")
+                (let* ((dep (plist-get result :dep))
+                       (file (plist-get result :file))
+                       (duration (plist-get result :duration))
+                       (elsa-cache-file (elsa--get-cache-file-name global-state dep)))
+                  (elsa-log
+                   (elsa--processing-line global-state "done" file worker-id duration))
+                  (load (f-no-ext elsa-cache-file) t t))
+                (cl-incf (oref global-state processed-file-index))
+                (setf (cdr worker-state) (plist-put (cdr worker-state) :ready t)))
+               ((equal op "load-from-cache")
+                (lgr-debug lgr "Worker %s loaded all cache files for this layer" worker-id)
+                (setf (cdr worker-state) (plist-put (cdr worker-state) :ready t)))))
+          (lgr-debug lgr "Async process done in worker %d, result: %s" worker-id result)
+          (setf (cdr worker-state) (plist-put (cdr worker-state) :ready t))
+          t)))))
 
 (defun elsa--wait-for-all (get-worker-states)
   (catch 'all-workers-ready
@@ -473,8 +520,12 @@ used by the LSP server to not reload already processed files."
         (elsa--wait-for-all (lambda () workers-state))
         (lgr-debug lgr "All workers updated global state for layer %s" i))
 
-      (--each workers
-        (async-send it :op "quit"))
+      (-each workers-state
+        (-lambda ((state &as worker-id))
+          (setf (cdr state) (plist-put (cdr state) :ready nil))
+          (let ((worker (nth worker-id workers)))
+            (async-send worker :op "quit"))))
+      (elsa--wait-for-all (lambda () workers-state))
       (lgr-debug lgr "All workers quit")
 
       (let ((start-time (current-time))
